aea06f7367dddb2c2f07419f10b488d618756b66
TravonteD <tman1300@aol.com>
2020-07-27 18:54:02 -0400

    refactor: add type restrictions

--- src/mpd.cr
+++ src/mpd.cr
@@ -7,7 +7,7 @@ struct Peridot::MPD
     @connection = LibMpdClient.mpd_connection_new(host, port, 1000) # Timeout is 1 second for now
   end
 
-  def state
+  def state : String | Nil
     case LibMpdClient.mpd_get_state(self.status)
     when LibMpdClient::MpdState::MPD_STATE_PLAY
       "playing"
@@ -22,39 +22,40 @@ struct Peridot::MPD
     end
   end
 
-  def volume 
+  def volume : Int32
     LibMpdClient.mpd_status_get_volume(self.status)
   end
 
-  def repeat 
+  def repeat? : Bool
     LibMpdClient.mpd_status_get_repeat(self.status)
   end
 
-  def random 
+  def random? : Bool
     LibMpdClient.mpd_status_get_random(self.status)
   end
 
-  def single 
+  def single? : Bool
     LibMpdClient.mpd_status_get_single(self.status)
   end
 
-  def consume 
+  def consume? : Bool
     LibMpdClient.mpd_status_get_consume(self.status)
   end
 
-  def elapsed_time 
+  def elapsed_time : Int32
     LibMpdClient.mpd_status_get_elapsed_time(self.status)
   end
 
-  def total_time 
+  def total_time : Int32
     LibMpdClient.mpd_status_get_total_time(self.status)
   end
 
-  def bit_rate 
+  # rate is in kilobits
+  def bit_rate : Int32
     LibMpdClient.mpd_status_get_bit_rate(self.status)
   end
 
-  private def status
+  private def status : LibMpdClient::MpdStatus*
     LibMpdClient.mpd_run_status(@connection)
   end
 end
@@ -62,6 +63,11 @@ end
 struct Peridot::MPD::Queue
   def initialize(connection : LibMpdClient::MpdConnection*)
     @connection = connection
+  end
+
+  def length : Int32
+    status = LibMpdClient.mpd_run_status(@connection)
+    LibMpdClient.mpd_status_get_queue_length(status)
   end
 end
 
