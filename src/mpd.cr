7548cbd6bc30b07aed0c49a8209dcdc383e97a68
TravonteD <tman1300@aol.com>
2020-07-27 18:40:10 -0400

    mpd: add structs for queue and songs

--- src/mpd.cr
+++ src/mpd.cr
@@ -6,4 +6,18 @@ struct Peridot::MPD
   def initialize(host : String, port : Int32)
     @connection = LibMpdClient.mpd_connection_new(host, port, 1000) # Timeout is 1 second for now
   end
+
+end
+
+struct Peridot::MPD::Queue
+  def initialize(connection : LibMpdClient::MpdConnection*)
+    @connection = connection
+  end
+end
+
+struct Peridot::MPD::Song
+  def initialize(connection : LibMpdClient::MpdConnection*, song : LibMpdClient::MpdSong*)
+    @connection = connection
+    @song = song
+  end
 end
