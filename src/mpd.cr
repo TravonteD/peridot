43598cd06b3962a829db696b0485cf2c2ad2be63
TravonteD <tman1300@aol.com>
2020-07-27 18:46:37 -0400

    mpd: add function for getting the current state

--- src/mpd.cr
+++ src/mpd.cr
@@ -7,6 +7,24 @@ struct Peridot::MPD
     @connection = LibMpdClient.mpd_connection_new(host, port, 1000) # Timeout is 1 second for now
   end
 
+  def state
+    case LibMpdClient.mpd_get_state(self.status)
+    when LibMpdClient::MpdState::MPD_STATE_PLAY
+      "playing"
+    when LibMpdClient::MpdState::MPD_STATE_PAUSE
+      "paused"
+    when LibMpdClient::MpdState::MPD_STATE_STOP
+      "stopped"
+    when LibMpdClient::MpdState::MPD_STATE_UNKNOWN
+      "unknown"
+    else
+      raise "Invalid state received from server"
+    end
+  end
+
+  private def status
+    LibMpdClient.mpd_run_status(@connection)
+  end
 end
 
 struct Peridot::MPD::Queue
