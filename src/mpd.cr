57d575413ea63ce337fbeb26bc2c923f7856e023
TravonteD <tman1300@aol.com>
2020-07-27 19:08:14 -0400

    mpd: add song tags and current_song

--- src/mpd.cr
+++ src/mpd.cr
@@ -1,14 +1,15 @@
 require "libmpdclient"
 
 struct Peridot::MPD
-  getter :connection
+  getter :queue
 
   def initialize(host : String, port : Int32)
     @connection = LibMpdClient.mpd_connection_new(host, port, 1000) # Timeout is 1 second for now
+    @queue = Queue.new(@connection)
   end
 
   def state : String | Nil
-    case LibMpdClient.mpd_get_state(self.status)
+    case LibMpdClient.mpd_status_get_state(self.status)
     when LibMpdClient::MpdState::MPD_STATE_PLAY
       "playing"
     when LibMpdClient::MpdState::MPD_STATE_PAUSE
@@ -55,6 +56,11 @@ struct Peridot::MPD
     LibMpdClient.mpd_status_get_bit_rate(self.status)
   end
 
+  def current_song : Song | Nil
+    song_id = LibMpdClient.mpd_status_get_song_id(self.status)
+    @queue.songs.find { |x| x.id == song_id }
+  end
+
   private def status : LibMpdClient::MpdStatus*
     LibMpdClient.mpd_run_status(@connection)
   end
@@ -65,9 +71,17 @@ struct Peridot::MPD::Queue
     @connection = connection
   end
 
-  def length : Int32
+  def length : UInt32
     status = LibMpdClient.mpd_run_status(@connection)
     LibMpdClient.mpd_status_get_queue_length(status)
+  end
+
+  def songs : Array(Peridot::MPD::Song)
+    songs = [] of Peridot::MPD::Song
+    (0..self.length - 1).each do |i|
+      songs << Peridot::MPD::Song.new(@connection, LibMpdClient.mpd_run_get_queue_song_pos(@connection, i))
+    end
+    songs
   end
 end
 
@@ -75,5 +89,55 @@ struct Peridot::MPD::Song
   def initialize(connection : LibMpdClient::MpdConnection*, song : LibMpdClient::MpdSong*)
     @connection = connection
     @song = song
+  end
+
+  def id : UInt32
+    LibMpdClient.mpd_song_get_id(@song)
+  end
+
+  def artist
+    tag(:artist)
+  end
+
+  def album
+    tag(:album)
+  end
+
+  def album_artist
+    tag(:album_artist)
+  end
+
+  def title
+    tag(:title)
+  end
+
+  def track
+    tag(:track)
+  end
+
+  def name
+    tag(:name)
+  end
+
+  def genre
+    tag(:genre)
+  end
+
+  def date
+    tag(:date)
+  end
+
+  private def tag(tag_name : Symbol) : String
+    tags = {
+      artist: LibMpdClient::MpdTagType::MPD_TAG_ARTIST,
+      album: LibMpdClient::MpdTagType::MPD_TAG_ALBUM,
+      album_artist: LibMpdClient::MpdTagType::MPD_TAG_ALBUM_ARTIST,
+      title: LibMpdClient::MpdTagType::MPD_TAG_TITLE,
+      track: LibMpdClient::MpdTagType::MPD_TAG_TRACK,
+      name: LibMpdClient::MpdTagType::MPD_TAG_NAME,
+      genre: LibMpdClient::MpdTagType::MPD_TAG_GENRE,
+      date: LibMpdClient::MpdTagType::MPD_TAG_DATE,
+    }
+    String.new(LibMpdClient.mpd_song_get_tag(@song, tags[tag_name], 0))
   end
 end
