587ddcbdae7144a6c10bcd987569b85090cda295
TravonteD <tman1300@aol.com>
2020-07-28 13:44:37 -0400

    refactor: fleshout the ui component

--- src/peridot.cr
+++ src/peridot.cr
@@ -4,89 +4,49 @@ require "log"
 
 Log.setup(:debug, Log::IOBackend.new(File.new("debug.log", "w")))
 
-def format_line_margin(start_string : String, end_string : String, width : Int32) : String
-    margin = " " * (width - (start_string.size + end_string.size) - 2)
-    start_string + margin + end_string
-end
-
-def calculate_window_dimensions(max_width : Int32, max_height : Int32)
-  status_width = queue_width = max_width - 2
-  library_width = playlist_width = 15
-  library_height = 5
-  queue_width -= library_width
-  status_height = 4
-  queue_height = max_height - 2 - status_height
-  playlist_height = max_height - 2 - status_height - library_height
-  {
-    playlist: {
-      x: 1,
-      y: library_height + 1,
-      w: playlist_width,
-      h: playlist_height
-    },
-    library: {
-      x: 1,
-      y: 1,
-      w: library_width,
-      h: library_height
-    },
-    queue: {
-      x: library_width + 1,
-      y: 1,
-      w: queue_width,
-      h: queue_height
-    },
-    status: {
-      x: 1,
-      y: max_height - status_height - 1,
-      w: status_width,
-      h: status_height
-    }
-  }
-end
-
 def main
   begin
     mpd = Peridot::MPD.new("localhost", 6600)
-    main_window = Peridot::UI.init
-
-    # Dimensions
-    dimensions = calculate_window_dimensions(main_window.width, main_window.height)
+    ui = Peridot::UI.new(mpd)
 
     # Library Window
     categories = ["Queue", "Songs", "Artists", "Albums"]
-    library_window = Peridot::UI::Window.new("Library", dimensions[:library])
     library_selected = 0
-    library_window.write_lines(categories, library_selected)
-    main_window << library_window.container
-
-    # Playlist Window
-    playlist_window = Peridot::UI::Window.new("Playlists", dimensions[:playlist])
-    main_window << playlist_window.container
+    ui.windows[:library].write_lines(categories, library_selected)
 
     # Queue Window
-    songs = mpd.queue.songs.map { |x| format_line_margin("#{x.artist} - #{x.title}", "#{x.album}", dimensions[:queue][:w]) }
     queue_selected = 0
-    queue_window = Peridot::UI::Window.new("Queue (#{songs.size} Songs)", dimensions[:queue])
-    queue_window.write_lines(songs, queue_selected)
-    main_window << queue_window.container
+    songs = ui.songs
+    ui.windows[:queue].write_lines(songs, queue_selected)
+    # Start with the queue window active
+    ui.windows[:queue].select
 
     # Status Window
-    status_window = Peridot::UI::Window.new(mpd.formatted_status, dimensions[:status])
-    status_window.write_lines(mpd.now_playing_stats)
-    main_window << status_window.container
+    ui.windows[:status].write_lines(mpd.now_playing_stats)
 
-    main_window.render
+    ui.render
 
     # Main Event loop
     loop do
-      ev = main_window.poll
+      ev = ui.poll
 
       case ev.type
       when Termbox::EVENT_KEY
         case ev.key
         when Termbox::KEY_CTRL_C, Termbox::KEY_CTRL_D
           break
+        when Termbox::KEY_CTRL_L
+          ui.windows[:queue].deselect
+          ui.windows[:playlist].deselect
+          ui.windows[:library].select
+        when Termbox::KEY_CTRL_Q
+          ui.windows[:library].deselect
+          ui.windows[:playlist].deselect
+          ui.windows[:queue].select
+        when Termbox::KEY_CTRL_P
+          ui.windows[:library].deselect
+          ui.windows[:queue].deselect
+          ui.windows[:playlist].select
         when Termbox::KEY_ENTER
           mpd.play(mpd.queue.songs[queue_selected].id)
         else
@@ -104,23 +64,23 @@ def main
           when 'j'
             queue_selected += 1 unless queue_selected == (songs.size - 1)
             # Rerender queue window
-            queue_window.write_lines(songs, queue_selected)
+            ui.windows[:queue].write_lines(songs, queue_selected)
           when 'k'
             queue_selected -= 1 unless queue_selected == 0
             # Rerender queue window
-            queue_window.write_lines(songs, queue_selected)
+            ui.windows[:queue].write_lines(songs, queue_selected)
           end
         end
       end
 
       # Rerender status window
-      status_window.add_title(mpd.formatted_status)
-      status_window.write_lines(mpd.now_playing_stats)
+      ui.windows[:status].add_title(mpd.formatted_status)
+      ui.windows[:status].write_lines(mpd.now_playing_stats)
 
-      main_window.render
+      ui.render
     end
   ensure
-    main_window.shutdown if main_window
+    ui.shutdown if ui
   end
 end
 
--- src/ui.cr
+++ src/ui.cr
@@ -2,96 +2,164 @@ require "termbox"
 
 include Termbox
 
-module Peridot::UI
-  # Creates a new window, sets up colors
-  # and adds a border
-  def self.init
-    w = Termbox::Window.new
-    w.set_output_mode(OUTPUT_NORMAL)
-    w.set_primary_colors(8, 0)
-    w.clear
-    w << Border.new(w)
-    w.render
-    w
+struct Peridot::UI
+  getter windows
+  getter songs : Array(String)
+
+  def initialize(mpd : Peridot::MPD)
+    @mpd = mpd
+    @w = Termbox::Window.new
+    @w.set_output_mode(OUTPUT_NORMAL)
+    @w.set_primary_colors(8, 0)
+    @w.clear
+    @w << Border.new(@w)
+
+    dimensions = calculate_window_dimensions
+    @songs = mpd.queue.songs.map { |x| format_line_margin("#{x.artist} - #{x.title}", "#{x.album}", dimensions[:queue][:w]) }
+    @windows = {} of Symbol => Window
+    @windows[:library] = Peridot::UI::Window.new("Library", dimensions[:library])
+    @windows[:playlist] = Peridot::UI::Window.new("Playlists", dimensions[:playlist])
+    @windows[:queue] = Peridot::UI::Window.new("Queue (#{songs.size} Songs)", dimensions[:queue])
+    @windows[:status] = Peridot::UI::Window.new(@mpd.formatted_status, dimensions[:status])
+    @windows.values.each { |x| @w << x.container }
   end
 
-  # Represents a termbox container used as a window within the UI
-  struct Window
-    getter container : Termbox::Container
+  def <<(arg)
+    @w << arg
+  end
 
-    def initialize(title : String, dimensions : NamedTuple(x: Int32, y: Int32, w: Int32, h: Int32))
-      @container = Container.new(Position.new(dimensions[:x], dimensions[:y]), dimensions[:w], dimensions[:h])
-      @border = Border.new(container)
-      @container << @border
-      @offset = 0
-      add_title(title)
+  def render
+    @w.render
+  end
+
+  def poll
+    @w.poll
+  end
+
+  def shutdown
+    @w.shutdown
+  end
+
+  def calculate_window_dimensions
+    max_width = @w.width
+    max_height = @w.height
+    status_width = queue_width = max_width - 2
+    library_width = playlist_width = 15
+    library_height = 5
+    queue_width -= library_width
+    status_height = 4
+    queue_height = max_height - 2 - status_height
+    playlist_height = max_height - 2 - status_height - library_height
+    {
+      playlist: {
+        x: 1,
+        y: library_height + 1,
+        w: playlist_width,
+        h: playlist_height
+      },
+      library: {
+        x: 1,
+        y: 1,
+        w: library_width,
+        h: library_height
+      },
+      queue: {
+        x: library_width + 1,
+        y: 1,
+        w: queue_width,
+        h: queue_height
+      },
+      status: {
+        x: 1,
+        y: max_height - status_height - 1,
+        w: status_width,
+        h: status_height
+      }
+    }
+  end
+
+  private def format_line_margin(start_string : String, end_string : String, width : Int32) : String
+      margin = " " * (width - (start_string.size + end_string.size) - 2)
+      start_string + margin + end_string
+  end
+end
+
+# Represents a termbox container used as a window within the UI
+struct Peridot::UI::Window
+  getter container : Termbox::Container
+
+  def initialize(title : String, dimensions : NamedTuple(x: Int32, y: Int32, w: Int32, h: Int32))
+    @container = Container.new(Position.new(dimensions[:x], dimensions[:y]), dimensions[:w], dimensions[:h])
+    @border = Border.new(container)
+    @container << @border
+    @offset = 0
+    add_title(title)
+  end
+
+  def write_line(text : String, line : Int32)
+    text.chars.each.with_index do |char, column|
+      @container << Cell.new(char, Position.new(column + 1, line))
+      column += 1
     end
+  end
 
-    def write_line(text : String, line : Int32)
-      text.chars.each.with_index do |char, column|
-        @container << Cell.new(char, Position.new(column + 1, line))
-        column += 1
-      end
+  # Writes the line using the given colors
+  def write_line(text : String, line : Int32, fg : Int32, bg : Int32)
+    text.chars.each.with_index do |char, column|
+      @container << Cell.new(char, Position.new(column + 1, line), fg, bg)
+      column += 1
     end
+  end
 
-    # Writes the line using the given colors
-    def write_line(text : String, line : Int32, fg : Int32, bg : Int32)
-      text.chars.each.with_index do |char, column|
-        @container << Cell.new(char, Position.new(column + 1, line), fg, bg)
-        column += 1
-      end
+  # Note: This will truncate the lines if they extend beyond the containers boundary
+  def write_lines(lines : Array(String))
+    clear
+
+    lines.each.with_index do |line, row|
+      write_line(line, row + 1)
+      break if row + 1 == @container.height - 2
     end
+  end
 
-    # Note: This will truncate the lines if they extend beyond the containers boundary
-    def write_lines(lines : Array(String))
-      clear
+  # Writes lines with the selected line highlighted
+  def write_lines(lines : Array(String), selected_index : Int32)
+    clear
 
-      lines.each.with_index do |line, row|
+    max_height = @container.height - 2
+    if (selected_index - @offset) == max_height
+      @offset += 1
+    end
+    if (selected_index - @offset) < 0
+      @offset -= 1 unless @offset == 0
+    end
+    selected_index -= @offset
+
+    lines[@offset..].each.with_index do |line, row|
+      if row == selected_index
+        write_line(line, row + 1, 2, 0)
+      else
         write_line(line, row + 1)
-        break if row + 1 == @container.height - 2
       end
+      break if row + 1 == max_height
     end
-
-    # Writes lines with the selected line highlighted
-    def write_lines(lines : Array(String), selected_index : Int32)
-      clear
-
-      max_height = @container.height - 2
-      if (selected_index - @offset) == max_height
-        @offset += 1
-      end
-      if (selected_index - @offset) < 0
-        @offset -= 1 unless @offset == 0
-      end
-      selected_index -= @offset
-
-      lines[@offset..].each.with_index do |line, row|
-        if row == selected_index
-          write_line(line, row + 1, 2, 0)
-        else
-          write_line(line, row + 1)
-        end
-        break if row + 1 == max_height
-      end
-    end
-
-    def add_title(title : String)
-      write_line(title, 0)
-    end
-
-    def select
-      @border.foreground = 2
-    end
-
-    def deselect
-      @border.foreground = 8
-    end
-
-    def clear
-      (0..@container.height).each.with_index do |line, row|
-          write_line((" " * (@container.width - 2)), row + 1)
-          break if row + 1 == @container.height - 2
-      end
-   end
   end
+
+  def add_title(title : String)
+    write_line(title, 0)
+  end
+
+  def select
+    @border.foreground = 2
+  end
+
+  def deselect
+    @border.foreground = 8
+  end
+
+  def clear
+    (0..@container.height).each.with_index do |line, row|
+        write_line((" " * (@container.width - 2)), row + 1)
+        break if row + 1 == @container.height - 2
+    end
+ end
 end
